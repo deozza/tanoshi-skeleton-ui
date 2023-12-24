ebb70660767e8711aff67d4ff22d99507d0eda65
Edenn Touitou <edenn.touitou@letsjam.fr>
2023-12-24 00:32:39 +0000

    init

---
+++ src/lib/atoms/TanoshiParagraph.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    import { TanoshiParagraphModel } from 'tansoshi-model/atoms/TanoshiParagraphModel.js'
+
+    export let tanoshiParagraphModel: TanoshiParagraphModel;
+
+</script>
+
+<p
+    class="text-{tanoshiParagraphModel.textAlignment}"
+>
+    <slot/>
+</p>
